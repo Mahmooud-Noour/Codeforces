9637d706f59129f054c8b2c1d1c5d3fc90b5f09a
MahmoudNour <ma7moudnour9@gmail.com>
2026-01-14 23:49:42 +0300

    Add 10 Codeforces Rating 800 solutions

--- Rating800/A_VanyaandFence/A_VanyaandFence.cpp
+++ Rating800/A_VanyaandFence/A_VanyaandFence.cpp
@@ -1,22 +1,20 @@
-
 #include <iostream>
 using namespace std;
+int n, h, sum, a[2001];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-int main()
-{
-	int n, h;
-	int Counter = 0;
-
-	cin >> n >> h;
-	while (n--)
-	{
-		int Length; cin >> Length;
-		if (Length <= h)
-			++Counter;
-		else
-			Counter += 2;
-	}
-	cout << Counter << endl;
-	return 0;
-
+    cin >> n >> h;
+    sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] > h)
+            sum += 2;
+        else
+            sum++;
+    }
+    cout << sum << endl;
+    return 0;
 }
